b6ae008647d8bf1484504a46ffafc8f3d7eceef9
FallenNocturne <32603473+FallenNocturne@users.noreply.github.com>
2022-02-12 19:20:19 +0000

    Change /register command to save phone number to database
    Saves phone number to user_database.db, either adding
    or updating it, based on whether it is already in the
    database or not. The response indicates which.

--- run_bot.py
+++ run_bot.py
@@ -1,8 +1,10 @@
 import discord
 import config
 import re
+from sqlalchemy import create_engine, text
 
 bot = discord.Bot()
+engine = create_engine("sqlite:///user_database.db")
 
 @bot.event
 async def on_ready():
@@ -28,8 +30,14 @@ async def on_message(message):
         if msg_text[0].upper() == "REGISTER":
             phone_num = msg_text[1]
             if re.match("[+]?\d+",phone_num):
-                await message.channel.send("Successfully registered number")
-                print(phone_num)
+                with engine.connect() as conn:
+                    result = conn.execute(text('SELECT * FROM userData WHERE userID == %s' % message.author.id))
+                    if result.all() != []:
+                        conn.execute(text('UPDATE userData SET phoneNo = "%s" WHERE userID == %s' % (phone_num,message.author.id)))
+                        await message.channel.send("Successfully updated number")
+                    else:
+                        conn.execute(text('INSERT INTO userData VALUES (%s,"%s")' % (message.author.id,phone_num)))
+                        await message.channel.send("Successfully registered number")
             else:
                 await message.channel.send("Invalid phone number")
         else:
