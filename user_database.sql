dafd172f277b4b79eeaa17025739da0db6735a99
Jeremy-Cheung <jeremycheung@hotmail.co.uk>
2022-02-12 23:34:39 +0000

    Add channel database
    Channel database added with foreign key using userID for the purpose
    of beaming messages through twilio

--- user_database.sql
+++ user_database.sql
@@ -2,3 +2,9 @@ CREATE TABLE userData (
     userID INT NOT NULL,
     phoneNo VARCHAR(20)
 )
+
+CREATE TABLE channelData (
+    channelID INT NOT NULL,
+    userID INT NOT NULL,
+    FOREIGN KEY (userID) REFERENCES userData (userID)
+)
