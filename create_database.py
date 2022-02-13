e9e043fcabe298abc3b080093428858c9136e154
FallenNocturne <32603473+FallenNocturne@users.noreply.github.com>
2022-02-13 07:50:32 +0000

    Fix errors in database generation programs
    Syntax and execution errors in user_database.sql
    and create_database.py have been fixed.

--- create_database.py
+++ create_database.py
@@ -3,6 +3,10 @@ engine = create_engine("sqlite:///user_database.db")
 with open("user_database.sql","r") as sql_file:
     lines = sql_file.readlines()
     transaction = "".join(lines)
+    transaction = transaction.split(";")
+    print(transaction)
     sql_file.close()
 with engine.connect() as conn:
-    conn.execute(text(transaction))
+    for command in transaction:
+        conn.execute(text(command))
+    conn.close()
--- user_database.sql
+++ user_database.sql
@@ -1,10 +1,11 @@
 CREATE TABLE userData (
     userID INT NOT NULL,
-    phoneNo VARCHAR(20)
+    phoneNo VARCHAR(20),
     PRIMARY KEY (userID)
 );
 
 CREATE TABLE channelData (
     channelID INT NOT NULL,
+    userID INT NOT NULL,
     FOREIGN KEY (userID) REFERENCES userData(userID)
-)
+);
